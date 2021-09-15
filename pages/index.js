7c39e8b5f0725e1b7034e7ae2c4fc6d03357e5c3
siegerts <stephen.siegert@gmail.com>
2021-09-15 13:45:53 -0400

    remove comment for testing

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,6 @@ import Head from 'next/head'
 // import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
-// update for headers
 
 export default function Home() {
   return (
